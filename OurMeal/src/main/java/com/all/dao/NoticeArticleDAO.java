5189de17bf37e97deb02d3d7089a6556275a1bb6
user <user@DESKTOP-C9BF406>
2018-11-13 00:46:23 +0900

    왕-게시판 DAO 수정

--- OurMeal/src/main/java/com/all/dao/NoticeArticleDAO.java
+++ OurMeal/src/main/java/com/all/dao/NoticeArticleDAO.java
@@ -1,5 +1,17 @@
 package com.all.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class NoticeArticleDAO {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	public void setSqlSession(SqlSessionTemplate sqlSession) {
+		this.sqlSession = sqlSession;
+	}
 
 }
