87ae29830cb727d08ff1b2e9f4a86cebb2b254df
k <k@DESKTOP-62UIMG6>
2018-11-13 00:14:15 +0900

    김종현 - 내일하자 피곤하다

--- OurMeal/src/main/java/com/controller/member/MemberLoginController.java
+++ OurMeal/src/main/java/com/controller/member/MemberLoginController.java
@@ -1,5 +1,8 @@
 package com.controller.member;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.all.model.Member;
+import com.service.member.LoginFailException;
 import com.service.member.MemberService;
+import com.all.model.User;
 
 @Controller
 public class MemberLoginController {
@@ -22,9 +27,36 @@ public class MemberLoginController {
 	}
 
 	@RequestMapping(value = "/memberLogin", method = RequestMethod.POST)
-	public String select(Model model, Member member) {
+	public String select(Model model, Member member, HttpServletRequest request) {
+		String id = member.getMember_id();
+		String password = member.getMember_pw();
 		
-		return "main/loginSuccessForm";
+		Map<String, Boolean> errors = new HashMap<>();
+		model.addAttribute("errors", errors);
+		
+		if(id == null || id.isEmpty()) {
+			errors.put("id", Boolean.TRUE);
+		}
+		
+		if(password == null || password.isEmpty()) {
+			errors.put("password", Boolean.TRUE);
+		}
+				
+		if(!errors.isEmpty()) {
+			return "main/loginSuccessForm";
+		}
+		
+		try {			
+			Member loginUser = service.memberLogin(member);
+			User user = new User(loginUser.getMember_id(), loginUser.getMember_pw());
+			
+			request.getSession().setAttribute("loginUser", user);			
+			
+			return "redirect:/";
+		}catch(LoginFailException e) {
+			errors.put("idOrPwNotMatch", Boolean.TRUE);
+			return "member/memberLoginForm";			
+		}		
 	}
 
 }
--- OurMeal/target/classes/WEB-INF/resources/include/popup.jsp
+++ OurMeal/target/classes/WEB-INF/resources/include/popup.jsp
@@ -1,9 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<div class="remodal-bg">
-  <a href="#modal">Modal 1</a><br>
-</div>
-
 <div class="remodal" data-remodal-id="modal" role="dialog" aria-labelledby="modal1Title" aria-describedby="modal1Desc">
   <button data-remodal-action="close" class="remodal-close" aria-label="Close"></button>
   <div>
@@ -12,9 +8,12 @@
 
     </p>
   </div>
-  <br>
-  <input type="text" class="remodal-text" name="member_id" value="아이디" ><br>
-  <input type="text" class="remodal-text" name="member_pw" value="패스워드"><br><br>
-  <!-- <button data-remodal-action="cancel" class="remodal-cancel">취소</button> -->
-  <button data-remodal-action="confirm" class="remodal-confirm">로그인</button>
+  <br>  
+  <form action="memberLogin" method="post" id="login_submit">
+	  <input type="text" class="remodal-text" id="member_id" name="member_id" value="아이디" ><br>
+	  <input type="text" class="remodal-text" id="member_pw" name="member_pw" value="패스워드"><br><br>
+	  <!-- <button data-remodal-action="cancel" class="remodal-cancel">취소</button> -->	  
+	  <button type="submit" data-remodal-action="confirm" class="remodal-confirm" id="login_btn">로그인</button>
+  </form>
+  
 </div>
