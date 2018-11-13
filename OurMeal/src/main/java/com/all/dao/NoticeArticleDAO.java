68d35077404a1287d36e92eac4d31d947c8ebc05
402-15 <402-15@DESKTOP-AE71AVB>
2018-11-13 11:37:27 +0900

    왕-게시판 작업중 중간 커밋 정상작동 확인

--- OurMeal/src/main/java/com/all/dao/NoticeArticleDAO.java
+++ OurMeal/src/main/java/com/all/dao/NoticeArticleDAO.java
@@ -4,14 +4,44 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.all.model.FreeArticle;
+import com.all.model.NoticeArticle;
+
 @Repository
 public class NoticeArticleDAO {
-	
+
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
+	private String strNameSpace = "com.jk.model.NoticeArticleMapper";
+
 	public void setSqlSession(SqlSessionTemplate sqlSession) {
 		this.sqlSession = sqlSession;
+	}
+
+	// 게시판 리스트
+	public NoticeArticle noticeList(NoticeArticle noticeArticle) {
+		return sqlSession.selectOne(strNameSpace + ".noticeList", noticeArticle);
+	}
+
+	// 게시글 보기
+	public NoticeArticle noticeContent(NoticeArticle noticeArticle) {
+		return sqlSession.selectOne(strNameSpace + ".noticeContent", noticeArticle);
+	}
+
+	// 게시글 작성
+	public int noticeWrite(NoticeArticle noticeArticle) {
+		return sqlSession.insert(strNameSpace + ".noticeWrite", noticeArticle);
+	}
+
+	// 게시글 수정
+	public int noticeUpdate(NoticeArticle noticeArticle) {
+		return sqlSession.update(strNameSpace + ".noticeUpdate", noticeArticle);
+	}
+
+	// 게시글 삭제
+	public int noticeDelete(NoticeArticle noticeArticle) {
+		return sqlSession.delete(strNameSpace + ".noticeDelete", noticeArticle);
 	}
 
 }
