d865a6fef8923f4c3cf08fe6e12837cf2a5f36d4
mj326 <mj326@DESKTOP-UD6KN51>
2018-11-21 18:25:38 +0900

    김민준

---
+++ OurMeal/src/main/java/com/controller/article/success/QnaMemberWriteSuccessController.java
@@ -0,0 +1,14 @@
+package com.controller.article.success;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class QnaMemberWriteSuccessController {
+	
+	@RequestMapping(value="/qnaMemberWriteSuccessListGo", method=RequestMethod.GET)
+	public String qnaMemberList() {
+		return "article/qnaMemberListForm";
+	}
+}
