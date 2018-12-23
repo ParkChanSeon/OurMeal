9fe40b35ca1e44c4dc62b0996ea5dfe02ba15091
k <k@DESKTOP-62UIMG6>
2018-12-23 22:33:47 +0900

    아이유 2018-12-23 앱 마이페이지 커밋

---
+++ OurMeal/src/main/java/com/controller/member/M_MyPageController.java
@@ -0,0 +1,67 @@
+package com.controller.member;
+
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.all.model.Member;
+import com.google.gson.Gson;
+import com.service.member.MemberService;
+
+@RestController
+public class M_MyPageController {
+	
+	@Autowired
+	private MemberService service;
+	
+	@RequestMapping(value="/myPage/select", method=RequestMethod.GET, produces="text/plain;charset=UTF-8")
+	public String SelectMypageUser() {
+		Member member = new Member();
+		
+		System.out.println("이곳을 오는지 확인");
+		
+		//앱에서 받은 아이디 정보를 넣어서 보내준다. 우선 임시로 아이디 값을 지정
+		String id = "test1";
+		member.setMember_id(id);
+		/*
+		String date = member.getMember_date().toString();
+		SimpleDateFormat dt = new SimpleDateFormat("yyyy-mm-dd")
+		*/
+		
+		member = service.myPage(member);
+
+		Gson gson = new Gson();
+		String strJson = gson.toJson(member);
+		return strJson;
+	}
+
+	@RequestMapping(value="/myPage/profile_update", method=RequestMethod.POST, produces="text/plain;charset=UTF-8")
+	public String UpdateMyprofile(String name, String email, String phone, String birth) throws ParseException {		
+		Member member = new Member();
+		
+		System.out.println(" 업데이트 시작");
+		//앱에서 받은 아이디 정보를 넣어서 보내준다. 우선 임시로 아이디 값을 지정
+		String id = "test1";
+			
+		member.setMember_id(id);
+		member.setMember_name(name);	
+		member.setMember_email(email);	
+		member.setMember_phone(phone);	
+		member.setMember_birth(birth);	
+				
+		System.out.println("업데이트 끝.");
+		
+		String result = String.valueOf(service.memberUpdate(member));		
+		
+		return result;
+	}
+	
+
+}
