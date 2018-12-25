7242a35a3875546857c1d1fa5ad2b1175d2a9621
k <k@DESKTOP-62UIMG6>
2018-12-25 22:34:06 +0900

    아이유 2018-12-25 마이페이지 작업중.

--- OurMeal/src/main/java/com/controller/member/M_MyPageController.java
+++ OurMeal/src/main/java/com/controller/member/M_MyPageController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.all.model.Health;
 import com.all.model.Member;
 import com.google.gson.Gson;
 import com.service.member.MemberService;
@@ -25,10 +26,10 @@ public class M_MyPageController {
 	public String SelectMypageUser() {
 		Member member = new Member();
 		
-		System.out.println("이곳을 오는지 확인");
+		System.out.println("앱 마이페이지 정보 출력");
 		
 		//앱에서 받은 아이디 정보를 넣어서 보내준다. 우선 임시로 아이디 값을 지정
-		String id = "test1";
+		String id = "TEST01";
 		member.setMember_id(id);
 		/*
 		String date = member.getMember_date().toString();
@@ -46,17 +47,15 @@ public class M_MyPageController {
 	public String UpdateMyprofile(String name, String email, String phone, String birth) throws ParseException {		
 		Member member = new Member();
 		
-		System.out.println(" 업데이트 시작");
+		System.out.println("앱 마이 페이지 업데이트");
 		//앱에서 받은 아이디 정보를 넣어서 보내준다. 우선 임시로 아이디 값을 지정
-		String id = "test1";
+		String id = "TEST01";
 			
 		member.setMember_id(id);
 		member.setMember_name(name);	
 		member.setMember_email(email);	
 		member.setMember_phone(phone);	
-		member.setMember_birth(birth);	
-				
-		System.out.println("업데이트 끝.");
+		member.setMember_birth(birth);
 		
 		String result = String.valueOf(service.memberUpdate(member));		
 		
@@ -66,13 +65,9 @@ public class M_MyPageController {
 	@RequestMapping(value="/myPage/password_update", method=RequestMethod.POST, produces="text/plain;charset=UTF-8")
 	public String UpdatePassword(String realpw, String pw, String pwre) throws ParseException {		
 		Member member = new Member();
-		
-		System.out.println(" 패스워드 업뎃 시작");
-		System.out.println(realpw);
-		System.out.println(pw);
-		System.out.println(pwre);
+		System.out.println("앱 마이 페이지 칼로리 정보 출력 및 인서트");
 		//앱에서 받은 아이디 정보를 넣어서 보내준다. 우선 임시로 아이디 값을 지정
-		String id = "test1";
+		String id = "TEST01";
 			
 		member.setMember_id(id);
 		member.setMember_pw(realpw);		
@@ -86,13 +81,46 @@ public class M_MyPageController {
 			member.setMember_pw(pw);
 			result = String.valueOf(service.memberPasswordUpdate(member));	
 		}else {
+			//기존 비밀번호가 틀릴때.
 			result = "0";
 		}
-						
-		System.out.println("패스워드 업뎃 끝.");
 			
 		return result;
 	}
 	
+	@RequestMapping(value="/myPage/Health", method=RequestMethod.POST, produces="text/plain;charset=UTF-8")
+	public String Health(String cm, String kg) {
+		Member member = new Member();		
+
+		//앱에서 받은 아이디 정보를 넣어서 보내준다. 우선 임시로 아이디 값을 지정
+		String id = "TEST01";
+		member.setMember_id(id);
+		
+		//SELECT 데이터가 있다면 member_health에 select 결과를 넣어준다.
+		Health member_health = null;
+		
+		System.out.println("키 정보" + cm);
+		System.out.println("몸무게 정보 " + kg);
+		
+		//앱에서 받은 정보를 모델에 넣어준다.
+		Health health = new Health();
+		health.setHealth_height(Double.parseDouble(cm));
+		health.setHealth_weight(Double.parseDouble(kg));		 
+		
+		String result = "0";
+		Gson gson = new Gson();
+		
+		health.setMember_id(member.getMember_id());
+		member_health = service.memberSelectHealth(health);		
+		
+		//insert
+		if(member_health==null) {			
+			result = String.valueOf(service.memberAddHealth(health));
+		}else {
+			result = gson.toJson(member_health);
+		}
+		
+		return result;
+	}
 
 }
