f270d79b781501d56dcec6c1b2e0faeac2e965a4
zzarungna <zzarungna@gmail.com>
2019-01-14 21:30:25 +0900

    아이유 2019-01-14 회원 가입 및 이것저것 수정.

--- OurMeal/src/main/java/com/controller/search/M_SearchController.java
+++ OurMeal/src/main/java/com/controller/search/M_SearchController.java
@@ -27,36 +27,40 @@ public class M_SearchController {
 	public String searchResult(String search) {
 		System.out.println("검색 조회 시작");
 		System.out.println("검색어 : " + search);
-		HashMap<String, Object> map = new HashMap<>() ;
 		
-		map.put("value", search);
-		map.put("allergy",0);
 		
-		List<Store> search_result_list = service.search(map);		
-		ArrayList<Store> storeList = new ArrayList();
-		
-		for(Store store : search_result_list) {
+		if(search.trim().length()>0) {
+			HashMap<String, Object> map = new HashMap<>() ;
 			
-			Star_bulletin review = new Star_bulletin();
-			review.setStore_code(store.getStore_code());
+			map.put("value", search);
+			map.put("allergy",0);
 			
-			int recordCount = reviewService.reviewCount(review);
-			store.setStore_reviewCount(recordCount);
-			storeList.add(store);
+
+			List<Store> search_result_list = service.search(map);
+			if(search_result_list!=null) {
+				ArrayList<Store> storeList = new ArrayList();
+				
+				for(Store store : search_result_list) {
+					
+					Star_bulletin review = new Star_bulletin();
+					review.setStore_code(store.getStore_code());
+					
+					int recordCount = reviewService.reviewCount(review);
+					store.setStore_reviewCount(recordCount);
+					storeList.add(store);
+				}
+				
+				Gson gson = new Gson();
+				
+				//검색 조회 끝
+				return gson.toJson(storeList);				
+			}else {
+				return null;
+			}
+	
+		}else {
+			return null;
 		}
-		
-		Gson gson = new Gson();
-		
-		//검색 조회 끝
-		return gson.toJson(storeList);
-		/*
-			가게 사진
-			가게 이름 ()평점
-			메뉴 종류
-			주소
-			
-			댓글수
-			끝 		 
-		 */		
+
 	}
 }
